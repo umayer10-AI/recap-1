bf6916632e7a715b94802e6b0ebdc148984b5097
umayer10-AI <mdumayer10@gmail.com>
2026-04-23 16:03:27 +0600

    add

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,11 +1,86 @@
+"use client";
 import React from 'react';
+import {Check} from "@gravity-ui/icons";
+import {Button, Description, FieldError, Form, Input, Label, TextField} from "@heroui/react";
+
 
 const page = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const onSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const userData = Object.fromEntries(formData.entries())
+    console.log(userData)
+
+    
+  };
+  return (
+    <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit}>
+
+
+        <TextField
+            isRequired
+            name="name"
+            validate={(value) => {
+              if (value.length < 3) {
+                return "Name must be at least 3 characters";
+              }
+              return null;
+            }}
+          >
+            <Label>Name</Label>
+            <Input name='name' placeholder="Enter name" />
+            <FieldError />
+          </TextField>
+
+      <TextField
+        isRequired
+        name="email"
+        type="email"
+        validate={(value) => {
+          if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+            return "Please enter a valid email address";
+          }
+          return null;
+        }}
+      >
+        <Label>Email</Label>
+        <Input name='email' placeholder="Enter email" />
+        <FieldError />
+      </TextField>
+
+      <TextField className="w-full max-w-[280px]" name="password">
+      <Label>Password</Label>
+      <InputGroup>
+        <InputGroup.Input
+          className="w-full max-w-[280px]"
+          type={isVisible ? "text" : "password"}
+          value={isVisible ? "87$2h.3diua" : "••••••••"}
+        />
+        <InputGroup.Suffix className="pr-0">
+          <Button
+            isIconOnly
+            aria-label={isVisible ? "Hide password" : "Show password"}
+            size="sm"
+            variant="ghost"
+            onPress={() => setIsVisible(!isVisible)}
+          >
+            {isVisible ? <Eye className="size-4" /> : <EyeSlash className="size-4" />}
+          </Button>
+        </InputGroup.Suffix>
+      </InputGroup>
+    </TextField>
+      <div className="flex gap-2">
+        <Button type="submit">
+          <Check />
+          Submit
+        </Button>
+        <Button type="reset" variant="secondary">
+          Reset
+        </Button>
+      </div>
+    </Form>
+  );
 };
 
 export default page;
