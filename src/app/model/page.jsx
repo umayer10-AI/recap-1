0283618bc3c5ba6e2d26e9241661be1170d0bea7
umayer10-AI <mdumayer10@gmail.com>
2026-04-23 12:26:02 +0600

    first commit

---
+++ src/app/model/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h2 className='text-center text-3xl font-bold my-10'>Model page</h2>
+        </div>
+    );
+};
+
+export default page;
